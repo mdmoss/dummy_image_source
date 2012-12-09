e0f648e5fce74e0cf5922d163b6877f7e317501c
Matthew Moss <mdm@cse.unsw.edu.au>
2012-12-10 09:18:15 +1100

    Add to dummy_image_source.py

--- scripts/dummy_image_source.py
+++ scripts/dummy_image_source.py
@@ -5,6 +5,8 @@ from sensor_msgs.msg import CompressedImage
 import glob
 import os
 from random import choice
+import time
+import math
 
 import config
 
@@ -13,27 +15,26 @@ def dummy_image_source():
     rospy.init_node('dummy_image_source')
     while not rospy.is_shutdown():
 
-        if rospy.has_param('image'):
+        if rospy.has_param('dummy_image_source/image'):
             try:
-                image_handle = load_image(rospy.get_param('image'))
+                image_handle = load_image(rospy.get_param('dummy_image_source/image'))
             except:
                 rospy.logwarn("Problem loading image") 
             else:
                 publish_image(pub, image_handle)
-        elif rospy.has_param('image_dir'):
+                print "Image Sent"
+        elif rospy.has_param('dummy_image_source/image_dir'):
             try:
-                image_handle = random_image_from_dir(rospy.get_param('image_dir'))
+                image_handle = random_image_from_dir(rospy.get_param('dummy_image_source/image_dir'))
             except:
                 rospy.logwarn("Problem loading image from directory") 
             else:
                 publish_image(pub, image_handle)
+                print "Image Sent"
         else:
             rospy.logwarn("No image source definied in 'image' or 'image_dir' params")
            
-        sleep_time = config.DEFAULT_SLEEP_TIME
-        if rospy.has_param('sleep_time'):
-            sleep_time = rospy.get_param('sleep_time')
-        rospy.sleep(sleep_time)
+        rospy.sleep(rospy.get_param('dummy_image_source/sleep_time', config.DEFAULT_SLEEP_TIME))
 
 def load_image(image):
     return open(image, 'rb')
@@ -42,6 +43,16 @@ def random_image_from_dir(directory):
     path = os.path.normpath(directory) + os.sep
     images = glob.glob(path + '*.jpg')
     return load_image(choice(image))
+
+def publish_image(pub, image_handle):
+    image = CompressedImage()
+    image.data = image_handle.read()
+    image.format = 'jpeg'
+    image.header.seq = 0
+    image.header.stamp.secs = math.floor(time.time())
+    image.header.stamp.nsecs = 0
+    image.header.frame_id = "0"
+    pub.publish(image)
 
 if __name__ == '__main__':
     try:
